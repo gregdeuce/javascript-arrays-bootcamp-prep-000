47fade867df909bfd23ae03676e10f706073bf27
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-25 00:26:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
  var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
     
        
-function addElementToBeginningofArray(arr, ele) {
-  
+function addElementToBeginningOfArray(arr, ele) {
+  return arr[...];
 }
     
