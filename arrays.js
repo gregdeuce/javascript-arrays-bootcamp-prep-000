eaa33d3334216dd256a9975ae12c496161a0badc
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-25 00:23:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,40 +1,7 @@
  var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
     
        
-    function addElementToBeginningOfArray(array, element) {
-      return ["foo", 1]
-    }
-    
-    function destructivelyAddElementToBeginningOfArray(array, element) {
-      return ['foo', 1]
-    }
-    
-    function addelementToEndOfArray(array, element) {
-      return [array.push]
-    }
-    
-    function destructivelyAddElementToEndOfArray(array, element) {
-      return ([...element] + [array])
-    }
-    
-    function accessElementInArray(array, index) {
-      return (array[index])
-    }
-    
-    function destructivelyRemoveElementFromBeginningOfArray(array) {
-      return (array.shift)
-    }
-    
-    function removeElementFromBeginningOfArray(array) {
-      return (array.slice(1))
-    }
-    
-    function destructivelyRemoveElementFromEndOfArray(array) {
-      return (array.slice[0])
-      console.log(array)
-    }
-    
-    function removeElementFromEndOfArray(array) {
-      return array.pop
-    }
+function addElementToBeginningofArray(arr, ele) {
+  
+}
     
