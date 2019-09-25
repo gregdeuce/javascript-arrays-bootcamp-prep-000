6d00b7b680a8f98c78779d90353d47d289a97694
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-25 03:49:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array = array.shift();
+  array.shift();
   return array;
 }
 
