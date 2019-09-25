6d7030ca6aa5018933bf889a33c4f974d9213e41
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-25 01:38:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,11 +2,13 @@
     
        
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+  [element, ...array];
+  return array;
 }
     
 function destructivelyAddElementToBeginningOfArray(array, element) {
-   return array.unshift(element);
+   array.unshift(element);
+   return array;
 }
 
 function addElementToEndOfArray(array, element) {
