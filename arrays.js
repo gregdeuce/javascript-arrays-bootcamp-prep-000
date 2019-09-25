bd755b001abed7142a795f0d0297163f66715e46
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-25 01:17:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,11 +10,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var arr = newArray();
+  
   return newArray([...array, element]);
 }
 
 function destructivelyAddelementToEndOfArray(array, element) {
-  var arr = newArray();
+  
   return newArray(array.pop(element));
 }
